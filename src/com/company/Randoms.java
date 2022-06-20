8961698eee4a7d7d55a431e86d8e6b3b4b959f90
MarinaBochkanova <mabo02@ya.ru>
2022-06-20 19:31:26 +0300

    one

---
+++ src/com/company/Randoms.java
@@ -0,0 +1,34 @@
+package com.company;
+import java.util.Iterator;
+import java.util.Random;
+
+
+public class Randoms implements Iterable<Integer> {
+
+    protected Random random = new Random();
+    int max;
+    int min;
+
+    public Randoms(int min, int max) {
+        this.max = max;
+        this.min = min;
+
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+
+                return random.nextInt(max - min) + min + 1;
+            }
+        };
+    }
+}
